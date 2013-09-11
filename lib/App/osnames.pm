b68e904140a1dcd2d8c81ff8bf13abcd465f0b95
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-09-11 13:40:38 +0700

    Initial commit

---
+++ lib/App/osnames.pm
@@ -0,0 +1,129 @@
+package App::osnames;
+
+use 5.010001;
+use strict;
+use warnings;
+
+# VERSION
+
+our $data = [
+
+    ['aix', [qw/unix sysv/], 'IBM AIX'],
+    ['beos', [qw//], 'See also: haiku'],
+    ['cygwin', [qw/unix/], ''],
+
+    ['darwin', [qw/unix bsd/],
+
+     'Mac OS X. Does not currently (2013) include iOS because Perl has not been
+     ported to that platform yet (but PerlMotion is being developed)',
+
+ ],
+
+    ['dec_osf', [qw//], 'DEC Alpha'],
+    ['dragonfly', [qw/unix bsd/], 'DragonFly BSD'],
+    ['freebsd', [qw/unix bsd/], ''],
+    ['gnukfreebsd', [qw/unix bsd/], 'Debian GNU/kFreeBSD'],
+    ['haiku', [qw//], 'See also: beos'],
+    ['hpux', [qw/unix sysv/], 'HP-UX'],
+    ['interix', [qw/unix/], ''],
+    ['irix', [qw/unix sysv/], ''],
+    ['linux', [qw/unix/], ''], # unix-like
+    ['MacOS', [qw//], 'Mac OS Classic (which predates Mac OS X)'],
+    ['midnightbsd', [qw/unix bsd/], ''],
+    ['minix', [qw/unix/], ''], # unix-like
+    ['mirbsd', [qw/unix bsd/], 'MirOS BSD'],
+
+    ['MSWin32', [qw//],
+
+     'All Windows platforms including 95/98/ME/NT/2000/XP/CE/.NET. But does not
+     include Cygwin (see "cygwin") or Interix (see "interix"). To get more
+     details on which Windows you are on, use Win32::GetOSName() or
+     Win32::GetOSVersion(). Ref: perlvar.',
+
+ ],
+
+    ['netbsd', [qw/unix bsd/], ''],
+    ['openbsd', [qw/unix bsd/], ''],
+    ['sco', [qw/unix sysv/], 'SCO UNIX'],
+    ['solaris', [qw/unix sysv/], 'This includes the old SunOS.'],
+
+    # These OS-es are listed on CPAN Testers OS Leaderboards, but I couldn't
+    # google any reports on them. So I couldn't peek the $Config{osname} value.
+
+    # - bigtrig
+    # - gnu hurd
+    # - os/2
+    # - os390/zos
+    # - qnx neutrino
+    # - tru64 (Tru64 UNIX, unix bsd)
+    # - vms
+
+];
+
+for (@$data) {
+    # unindent & unwrap text first, Text::Wrap doesn't do those
+    $_->[2] =~ s/^[ \t]+//mg;
+    $_->[2] =~ s/\n(\n?)(\S)/$1 ? "\n\n$2" : " $2"/mge;
+}
+
+# dump: display data as table
+#use Data::Format::Pretty::Text qw(format_pretty);
+#say format_pretty($data, {
+#    table_column_formats=>[{description=>[[wrap=>{columns=>40}]]}],
+#    table_column_orders=>[[qw/code summary description/]],
+#});
+
+# debug: dump data
+#use Data::Dump::Color;
+#dd $data;
+
+use Perinci::Sub::Gen::AccessTable qw(gen_read_table_func);
+
+my $res = gen_read_table_func(
+    name       => 'list_osnames',
+    summary    => 'A collection of possible $^O ($OSNAME) values, '.
+        'along with description',
+    description => <<'_',
+
+This list might be useful when coding, e.g. when you want to exclude or include
+certain OS (families) in your application/test.
+
+_
+    table_data => $data,
+    table_spec => {
+        summary => 'List of possible $^O ($OSNAME) values',
+        fields  => {
+            value => {
+                schema   => 'str*',
+                index    => 0,
+                sortable => 1,
+            },
+            tags => {
+                schema   => [array => of => 'str*'],
+                index    => 1,
+            },
+            description => {
+                schema   => 'str*',
+                index    => 2,
+            },
+        },
+        pk => 'value',
+    },
+);
+die "Can't generate list_osnames function: $res->[0] - $res->[1]"
+    unless $res->[0] == 200;
+
+1;
+# ABSTRACT: List possible $^O ($OSNAME) values
+
+=head1 SEE ALSO
+
+L<perlvar>
+
+L<Config>
+
+L<Devel::Platform::Info>
+
+The output of C<perl -V>
+
+=cut
